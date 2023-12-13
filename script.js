a9c00739131ddef30826cd959458b2ed3e9ce128
Matt Lightfoot <m-lightfoot@hotmail.com>
2023-12-13 12:23:38 +0000

    Added functionality to save button so your movie list gets saved to local storage

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+let arrayOfMovies;
+
+if (arrayOfMovies === undefined) {
+  arrayOfMovies = [];
+}
+
+arrayOfMovies = JSON.parse(localStorage.getItem("local-storage-data"));
+
+for (i = 0; i < arrayOfMovies.length; i++) {
+  console.log(arrayOfMovies[i].title)
+
+  const tableRow = $('<tr>');
+
+  const title = $('<td>');
+  title.text(arrayOfMovies[i].title);
+
+  const year = $('<td>');
+  year.text(arrayOfMovies[i].year);
+
+  const actors = $('<td>');
+  actors.text(arrayOfMovies[i].actors);
+
+  const plot = $('<td>');
+  plot.text(arrayOfMovies[i].plot);
+
+  const poster = $('<td id="posters">');
+  poster.css('text-align', 'center');
+  const img = $('<img id="posterImg">');
+  img.attr('src', arrayOfMovies[i].poster);
+  img.css('height', '200px');
+  poster.append(img);
+
+  tableRow.append(title, year, actors, plot, poster);
+
+  const tbody = $('.table tbody');
+  tbody.append(tableRow);
+}
+
 function chooseMovie(movie) {
 
   var queryURL = "https://www.omdbapi.com/?t=" + movie + "&apikey=trilogy";
@@ -40,6 +78,18 @@ function chooseMovie(movie) {
       const tbody = $('.table tbody');
       tbody.append(tableRow);
 
+      const movieForArray = {
+        title: data.Title,
+        year: data.Year,
+        actors: data.Actors,
+        plot: data.Plot,
+        poster: data.Poster
+      }
+
+      arrayOfMovies.push(movieForArray);
+
+      console.log(arrayOfMovies);
+
     })
     .catch(function (response) {
     });
@@ -56,6 +106,8 @@ const saveMessage = $(".saveMessage");
 
 saveBtn.on('click', () => {
   saveMessage.text("Your movie list has been saved!");
+
+  localStorage.setItem("local-storage-data", JSON.stringify(arrayOfMovies));
 
   function emptyMessage() {
     saveMessage.text("");
